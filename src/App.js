d628183f559d2f894024e3bc14aa5fffd49ce337
Anisxaman <83417019+Anisxaman@users.noreply.github.com>
2021-10-03 22:58:31 +0600

    start

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>This is new react app</h1>
+
+
+
+
+
+      
+    </div>
+  );
+}
+
+export default App;
